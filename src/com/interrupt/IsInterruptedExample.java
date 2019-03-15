3c14303f5a7c56fdb8eac638e352c438d0e50e86
Manohar51 <manohar.glimpse@gmail.com>
2019-03-15 18:04:38 +0530

    Commit in Threads

--- src/com/interrupt/IsInterruptedExample.java
+++ src/com/interrupt/IsInterruptedExample.java
@@ -4,11 +4,11 @@ class SampleThread extends Thread {
 	@Override
 	public void run() {
 		for (int i = 1; i <= 5; i++) {
-			//if (Thread.interrupted()) { // it will make current thread interrupted state to false if it is true, after this ckeck
+			//if (Thread.interrupted()) { // it will make current thread interrupted state to false if it is true, after this check
 			if (isInterrupted()) { // it will give true or false according to condition
-				System.out.println(Thread.currentThread().getName() + " is interrupted!!!" + i);
+				System.out.println(Thread.currentThread().getName() + " is interrupted!!! " + i);
 			} else {
-				System.out.println(Thread.currentThread().getName() + " is Not!!!" + i);
+				System.out.println(Thread.currentThread().getName() + " is Not!!! " + i);
 			}
 		}
 	}
